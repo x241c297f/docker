f0f8304abb8bbd123f1478e2e895440d53dbe057
Wolfgang Hölzl <138807728+x241c297f@users.noreply.github.com>
2024-10-16 17:39:03 +0200

    Add common development base image
    Signed-off-by: Wolfgang Hölzl <138807728+x241c297f@users.noreply.github.com>

---
+++ common-dev-base/build.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd "$SCRIPT_DIR" || ( echo "Error: Cannot enter $SCRIPT_DIR"  && exit 1 )
+
+USERNAME=x241c297f
+USERID=$(id -u)
+GROUPNAME=$(id -gn)
+GROUPID=$(id -g)
+
+IMAGE_NAME=common-dev-base
+docker build \
+    --pull \
+    -t x241c297f/${IMAGE_NAME}:latest \
+    -f ./Dockerfile \
+    --build-arg USERNAME=$USERNAME \
+    --build-arg USERID="$USERID" \
+    --build-arg GROUPNAME="$GROUPNAME" \
+    --build-arg GROUPID="$GROUPID" \
+    "$@" \
+    .
+
+docker push x241c297f/${IMAGE_NAME}:latest
